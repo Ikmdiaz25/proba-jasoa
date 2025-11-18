bb75c0e5f293425147550f3c0dbe0ad92fb2a70c
Michael <ikmdiaz25@lhusurbil.eus>
2025-11-18 11:08:43 +0100

    bukatu kommit

--- ZureTaldekideLiderIzena/AplikazioaTest.java
+++ ZureTaldekideLiderIzena/AplikazioaTest.java
@@ -1,6 +1,13 @@
 package ZureTaldekideLiderIzena;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import java.io.*;
+import java.util.ArrayList;
+
 public class AplikazioaTest{
+
+@Test
 
 
 
