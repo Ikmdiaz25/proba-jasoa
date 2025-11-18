4e38436c3e626f2e3645270e8b5359afc42c4fc7
Michael <ikmdiaz25@lhusurbil.eus>
2025-11-18 10:25:07 +0100

    docs aplikatzen

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa","l":"SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa()","u":"%3Cinit%3E()"}];updateSearchResults();
